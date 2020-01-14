d15b7ea913abf168ce1cc6fe046ae70212d825d0
changjiale <17637941480@163.com>
2020-01-14 10:58:27 +0800

    update discovery

--- src/main/java/com/da/server/App.java
+++ src/main/java/com/da/server/App.java
@@ -2,11 +2,18 @@ package com.da.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class App {
+public class App extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
