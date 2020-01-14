4f200f3396561273b060798cef7acf2fef6cfa14
changjiale <17637941480@163.com>
2020-01-14 10:33:28 +0800

    create discovery

---
+++ src/main/java/com/da/server/demo/DaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.da.server.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/da/server/demo/DaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.da.server.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
